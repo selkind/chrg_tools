51a4509314fbcf6fb3058cf61c3b28303e810b3b
Samuel Elkind <samuel.elkind@gmail.com>
2021-02-14 13:19:50 -0700

    refactor into module in prepration for data migration

--- hearings/table_config.sql
+++ hearings/table_config.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS members(
     id integer PRIMARY KEY,
     metadata json,
     committee_membership json
-    );
+);
 
 CREATE TABLE IF NOT EXISTS hearings(
     id text PRIMARY KEY,
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS hearings(
     witness_meta json,
     member_meta json,
     parsed json
-    );
+);
 
 DO
 $$
@@ -43,4 +43,25 @@ CREATE TABLE IF NOT EXISTS committees(
     name text,
     chamber chamber,
     code text
-    );
+);
+
+CREATE TABLE IF NOT EXISTS representatives(
+    id integer PRIMARY KEY,
+    congress integer,
+    majority_code
+
+);
+CREATE TABLE IF NOT EXISTS commitee_assignments(
+    id SERIAL PRIMARY KEY,
+    congress integer,
+    code integer REFERENCES committees,
+    rep_id integer REFERENCES representatives,
+    assign_date date,
+    termination_date date,
+    period_of_service integer,
+    committee_status_congress_end integer,
+    committee_continuity_next_congress integer,
+    appointment_citation text,
+    committee_name text,
+    notes text
+);
