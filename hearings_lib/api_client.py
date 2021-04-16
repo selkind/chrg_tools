7fd57c34b0d8ae00822de5b223814d52640bffd4
Samuel Elkind <samuel.elkind@gmail.com>
2021-04-16 15:11:14 -0600

    beginning iterative requests to pull all package ids

--- hearings_lib/api_client.py
+++ hearings_lib/api_client.py
@@ -1,15 +1,27 @@
 import requests
+import datetime
+from typing import Optional, Dict
 
 
 class APIClient:
-    BASE_URL = 'https://api.govinfo.gov'
-    COLLECTION_LIST_ENDPOINT = f'{BASE_URL}/collections'
-    DEFAULT_LAST_MODIFIED_START_DATE = '1776-01-01'
-    CHRG_ENDPOINT = f'{COLLECTION_LIST_ENDPOINT}/CHRG/{DEFAULT_LAST_MODIFIED_START_DATE}'
+    BASE_URL: str = 'https://api.govinfo.gov'
+    COLLECTION_LIST_ENDPOINT: str = f'{BASE_URL}/collections'
+    DEFAULT_LAST_MODIFIED_START_DATE: str = f'{datetime.datetime(1776, 1, 1, 0, 0, 0).isoformat()}Z'
+    CHRG_ENDPOINT: str = f'{COLLECTION_LIST_ENDPOINT}/CHRG/{DEFAULT_LAST_MODIFIED_START_DATE}'
+    DEFAULT_PAGE_SIZE: int = 100
 
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self, api_key: str):
+        self.api_key: str = api_key
 
-    def _get(self, url, params={}):
+    def _get(self, url: str, params: Optional[Dict[str, str]] = {}) -> requests.Response:
         params['api_key'] = self.api_key
         return requests.get(url, params=params)
+
+    def get_hearings_from_congress(self, congress: int):
+        params = {
+            'offset': str(0),
+            'pageSize': str(self.DEFAULT_PAGE_SIZE),
+            'congress': str(congress)
+        }
+
+        return self._get(self.CHRG_ENDPOINT, params=params)
--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,10 +1,13 @@
+import os
 import pytest
 import responses
+from hearings_lib.hearings.load_project_env import ProjectEnv
 from hearings_lib.api_client import APIClient
 
 
 class TestAPIClient:
     TEST_API_KEY = "1234abc"
+
     @pytest.fixture
     def mocked_responses(self):
         with responses.RequestsMock() as rsps:
@@ -25,3 +28,19 @@ class TestAPIClient:
         resp = api_client._get(api_client.COLLECTION_LIST_ENDPOINT)
         assert resp.status_code == 200
         assert mocked_responses.calls[0].request.params == {'api_key': self.TEST_API_KEY}
+
+    def test_api_response_for_huge_offset(self):
+        ProjectEnv.load_env()
+        api_client = APIClient(os.getenv('GPO_API_KEY'))
+        assert api_client.DEFAULT_LAST_MODIFIED_START_DATE == "1776-01-01T00:00:00Z"
+        params = {
+            'offset': str(5000),
+            'pageSize': str(api_client.DEFAULT_PAGE_SIZE),
+            'congress': str(116)
+        }
+
+        empty_response = api_client._get(api_client.CHRG_ENDPOINT, params=params)
+        assert empty_response.status_code == 200
+        r_json = empty_response.json()
+        assert r_json['message'] == 'No results found'
+        assert r_json['nextPage'] is None
