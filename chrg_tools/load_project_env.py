514a574f32c84ce976e4bf03309bcbff412f64a8
Samuel Elkind <samuel.elkind@gmail.com>
2021-05-08 19:55:15 -0600

    rename directory in anticipation of switching to public

--- chrg_tools/load_project_env.py
+++ chrg_tools/load_project_env.py
@@ -5,10 +5,11 @@ import os
 
 
 def load_project_env(working_dir: str = '') -> Dict:
-    """Loads environment variables from .env file and api/db configuration values from config.toml in specified directory (current working directory by default).
+    """Loads environment variables from .env file and api/db configuration values from config.toml
+    in the specified directory (current working directory by default).
     These files should be modeled after example-env and example-config.toml, Matching the keys in the files is required.
 
-    :param working_dir: (optional) String, The path to the directory containing the .env and config.toml 
+    :param working_dir: (optional) String, The path to the directory containing the .env and config.toml
     :type working_dir: str
 
     :return: Nested Dictionaries, The values specified in the config.toml file
